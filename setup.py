2228fde73158ea771b222e230e40827933807d16
Ng Zhi An <ngzhian@gmail.com>
2015-06-12 00:16:05 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='colordiffs',
+    version='1.0',
+    description='Syntax highlights for your git diffs',
+    author='Ng Zhi An',
+    author_email='ngzhian@gmail.com',
+    url='https://github.com/ngzhian/colordiffs',
+    scripts=['colordiffs.py']
+)
