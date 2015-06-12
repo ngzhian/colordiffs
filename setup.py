72062ec3f2e94c58ef20588eb362a2b730bd9626
Ng Zhi An <ngzhian@gmail.com>
2015-06-12 04:00:34 +0000

    v0.1.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ def readme():
 
 setup(
     name='colordiffs',
-    version='0.1.0',
+    version='0.1.1',
     description='Syntax highlights for your git diffs',
     author='Ng Zhi An',
     author_email='ngzhian@gmail.com',
