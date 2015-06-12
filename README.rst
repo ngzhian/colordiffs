d4ca980466fa23298d5286241910f10ba74c8990
Ng Zhi An <ngzhian@gmail.com>
2015-06-12 03:16:57 +0000

    Update setup.py, specify requirements, use README.rst

---
+++ README.rst
@@ -0,0 +1,35 @@
+==========
+Colordiffs
+==========
+
+Colors for your git diffs
+
+How to run
+==========
+
+::
+
+    git diff HEAD^ | python colordiffs.py | less -r
+
+.. |screenshot| image:: https://github.com/ngzhian/colordiffs/raw/master/colordiffs.png
+    :alt: colordiffs screenshot
+
+Installation
+============
+
+::
+
+    pip install colordiffs
+
+Requirements
+============
+
+::
+
+    Python 2.x
+    Pygments
+
+TODO
+====
+
+Python 3 (this shouldn't be difficult)
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 
 def readme():
-    f = open('README.md')
+    f = open('README.rst')
     info = f.read()
     f.close()
     return info
@@ -12,11 +12,24 @@ def readme():
 
 setup(
     name='colordiffs',
-    version='1.0',
+    version='0.1.0',
     description='Syntax highlights for your git diffs',
     author='Ng Zhi An',
     author_email='ngzhian@gmail.com',
     url='https://github.com/ngzhian/colordiffs',
     scripts=['colordiffs.py'],
     long_description=readme(),
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ],
+    install_requires=[
+        "Pygments>=2.0.2",
+    ]
 )
