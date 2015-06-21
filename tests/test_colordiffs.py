2ff9a013c37185a2dd2a6964903641836cb1172f
Ng Zhi An <ngzhian@gmail.com>
2015-06-21 20:20:41 +0000

    Add test case for stripping new lines

--- tests/test_colordiffs.py
+++ tests/test_colordiffs.py
@@ -3,18 +3,32 @@ import unittest
 from colordiffs import colordiffs
 
 
-def mock_file_contents(git_obj, filename):
-    with open('./failingoutput_1' + '_' + git_obj) as f:
-        diff = f.read()
-    return diff
+def mock_file_contents(test_file):
+    def mock(git_obj, filename):
+        with open(test_file + '_' + git_obj) as f:
+            diff = f.read()
+        return diff
+    return mock
 
 
 class TestColorDdiffs(unittest.TestCase):
-    def test_case(self):
-        with open('./failingoutput_1') as f:
+    def test_case_failingoutput_1(self):
+        test_file = './failingoutput_1'
+
+        with open(test_file) as f:
             diff = f.readlines()
 
-        colordiffs.file_contents_at_commit = mock_file_contents
+        colordiffs.file_contents_at_commit = mock_file_contents(test_file)
+
+        colordiffs.run(diff)
+
+    def test_case_failingoutput_2(self):
+        """test case from Jedi's code"""
+        test_file = './failcase'
+        with open(test_file) as f:
+            diff = f.readlines()
+
+        colordiffs.file_contents_at_commit = mock_file_contents(test_file)
 
         colordiffs.run(diff)
 
